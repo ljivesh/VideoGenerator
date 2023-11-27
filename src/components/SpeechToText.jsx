907046657dfe982f8998ef32eb7e6af1b9e942db
Menma <menma@pop-os.localdomain>
2023-11-27 16:54:09 +0530

    modified layot and debugging features

--- src/components/SpeechToText.jsx
+++ src/components/SpeechToText.jsx
@@ -1,14 +1,13 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../providers/Auth";
 import { useConversation } from "../customhooks/conversation-hook";
-import { useMicrophone, useRecognizer } from "../customhooks/recognizer-hook";
+import { useRecognizer } from "../customhooks/recognizer-hook";
 import { useSynthesize } from "../customhooks/synthesizer-hook";
 import { useQueue } from "@uidotdev/usehooks";
 import {
   ResultReason,
   CancellationReason,
 } from "microsoft-cognitiveservices-speech-sdk";
-import axios from "axios";
 import { SOCKETURL } from "../modules/envirnoment";
 import { useSpeechConfig } from "../modules/token_util";
 import {io} from 'socket.io-client';
@@ -35,7 +34,6 @@ function SpeechToText() {
   const { speechRecognizerRef, listening, setListening, speakHandler } =
     useRecognizer(speechConfig);
 
-    const {audioLevel, startAudioContext} = useMicrophone();
 
   const { speechSynthesizer, player, stopPlayer } = useSynthesize(speechConfig);
 
@@ -45,16 +43,6 @@ function SpeechToText() {
     addToQueue("You added a very big string in the queue, I am currently speaking this string");
   }
 
-  const sendMessage = async ()=> {
-    const message = "Hello from the client";
-    try {
-      
-      const resposne = await axios.post('api/speech/send-through-sse', {message});
-      console.log(resposne.data);
-    } catch(error) {
-      console.log(error);
-    }
-  };
 
   useEffect(() => {
     if (speechRecognizerRef.current) {
@@ -133,46 +121,6 @@ function SpeechToText() {
     }
   }, [stopFlag, player, clearQueue, disableModel, enableModel]);
 
-  // const stopPlayer = ()=> {
-  //   console.log(player)
-  // }
-
-  // useEffect(() => {
-  //   // Establish SSE connection
-  //   const eventSource = new EventSource(`${BASEURL}/api/speech/sse`, {withCredentials: true});
-  //   console.log(eventSource);
-
-  //   // Event listener for messages from the server
-  //   eventSource.onmessage = (event) => {
-  //     const newData = (event.data);
-  //     addToQueue(newData);
-  //     console.log(`New Data: ${newData}`);
-  //   };
-
-  //   // Set the user ID when receiving the welcome message
-  //   eventSource.onopen = (event) => {
-  //     console.log(`event data is: ${JSON.stringify(event)}`);
-      
-  //     if (event.data) {
-  //       const welcomeMessage = JSON.parse(event.data);
-  //       console.log("Welcome Message: ", welcomeMessage);
-  //     }
-      
- 
-  //   };
-    
-
-  //   // Event listener for errors
-  //   eventSource.onerror = (event) => {
-  //     console.error('Error occurred:', event);
-  //     eventSource.close();
-  //   };
-
-  //   // Cleanup on component unmount
-  //   return () => {
-  //     eventSource.close();
-  //   };
-  // }, []); // Empty dependency array ensures the effect runs only once
 
   useEffect(()=> {
     socket.on('connect', ()=> {
@@ -233,9 +181,6 @@ useEffect(()=> {
       ))}
       <button onClick={logout}>Logout</button>
       <button onClick={enQueue}>Enqueue</button>
-      <button onClick={sendMessage}>Send Message</button>
-      <button onClick={startAudioContext}>Start Audio Context</button>
-      <p>{audioLevel}</p>
     </>
   );
 }
